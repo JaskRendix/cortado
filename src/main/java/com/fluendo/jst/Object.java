8b92c3d41c62fbd7d8cb8b1127244c937935035d
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-11 12:43:44 +0200

    modernize Object, clean up code formatting, etc

--- src/main/java/com/fluendo/jst/Object.java
+++ src/main/java/com/fluendo/jst/Object.java
@@ -18,62 +18,65 @@
 
 package com.fluendo.jst;
 
-public abstract class Object
-{
+public abstract class Object {
+
   protected String name;
-  protected com.fluendo.jst.Object parent;
+  protected Object parent;
   protected int flags;
 
-  public static final int OBJECT_FLAG_LAST = (1 << 4);
+  public static final int OBJECT_FLAG_LAST = 1 << 4;
 
-  public synchronized String getName () {
-    return name;
-  }
-  public synchronized void setName (String newName) {
-    name = newName;
-  }
-  public Object() {
+  protected Object() {
     this("unnamed");
   }
-  public Object(String name) {
+
+  protected Object(String name) {
     super();
     this.name = name;
-    parent = null;
+    this.parent = null;
   }
-  public synchronized boolean setParent (com.fluendo.jst.Object newParent) {
-    if (parent != null)
+
+  public synchronized String getName() {
+    return name;
+  }
+
+  public synchronized void setName(String name) {
+    this.name = name;
+  }
+
+  public synchronized boolean setParent(Object newParent) {
+    if (parent != null) {
       return false;
-
+    }
     parent = newParent;
-
     return true;
   }
-  public synchronized com.fluendo.jst.Object getParent () {
+
+  public synchronized Object getParent() {
     return parent;
   }
-  public synchronized void unParent () {
+
+  public synchronized void unParent() {
     parent = null;
   }
 
-  public synchronized void setFlag(int flag)
-  {
+  public synchronized void setFlag(int flag) {
     flags |= flag;
   }
-  public synchronized void unsetFlag(int flag)
-  {
+
+  public synchronized void unsetFlag(int flag) {
     flags &= ~flag;
   }
-  public synchronized boolean isFlagSet(int flag)
-  {
+
+  public synchronized boolean isFlagSet(int flag) {
     return (flags & flag) == flag;
   }
 
-  public synchronized boolean setProperty(String name, java.lang.Object value)
-  {
+  public synchronized boolean setProperty(String name, java.lang.Object value) {
     return false;
   }
-  public synchronized java.lang.Object getProperty(String name)
-  {
+
+  public synchronized java.lang.Object getProperty(String name) {
     return null;
   }
 }
