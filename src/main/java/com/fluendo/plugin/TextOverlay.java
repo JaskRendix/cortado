f61c9871daa1ce6a6f9d52e6d9f6c751beefb49b
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-11 10:58:34 +0200

    modernize TextOverlay for BufferedImage and standard logger

--- src/main/java/com/fluendo/plugin/TextOverlay.java
+++ src/main/java/com/fluendo/plugin/TextOverlay.java
@@ -18,14 +18,23 @@
 
 package com.fluendo.plugin;
 
-import java.awt.*;
-import java.awt.image.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageProducer;
+import java.util.logging.Logger;
 
 /**
  * This class displays a simple text string on top of incoming video.
  */
 public class TextOverlay extends Overlay {
-    private int font_size = -1;
+    private static final Logger LOGGER = Logger.getLogger(TextOverlay.class.getName());
+
+    private int fontSize = -1;
     private Font font = null;
     private String text = null;
 
@@ -38,63 +47,82 @@ public class TextOverlay extends Overlay {
      */
     @Override
     protected void overlay(com.fluendo.jst.Buffer buf) {
-        Image img;
+        BufferedImage img;
 
-        /* img retrieval from VideoSink.java */
-        if (buf.object instanceof ImageProducer) {
-            img = component.createImage((ImageProducer) buf.object);
-        } else if (buf.object instanceof Image) {
-            img = (Image) buf.object;
+        if (buf.object instanceof BufferedImage bufferedImage) {
+            img = bufferedImage;
+        } else if (buf.object instanceof ImageProducer imageProducer) {
+            Image awtImg = component.createImage(imageProducer);
+            if (awtImg instanceof BufferedImage bufferedImage) {
+                img = bufferedImage;
+            } else {
+                img = new BufferedImage(
+                    component.getWidth() > 0 ? component.getWidth() : 320,
+                    component.getHeight() > 0 ? component.getHeight() : 240,
+                    BufferedImage.TYPE_INT_RGB
+                );
+                Graphics2D g2d = img.createGraphics();
+                g2d.drawImage(awtImg, 0, 0, null);
+                g2d.dispose();
+            }
+        } else if (buf.object instanceof Image awtImg) {
+            if (awtImg instanceof BufferedImage bufferedImage) {
+                img = bufferedImage;
+            } else {
+                img = new BufferedImage(
+                    awtImg.getWidth(null) > 0 ? awtImg.getWidth(null) : 320,
+                    awtImg.getHeight(null) > 0 ? awtImg.getHeight(null) : 240,
+                    BufferedImage.TYPE_INT_RGB
+                );
+                Graphics2D g2d = img.createGraphics();
+                g2d.drawImage(awtImg, 0, 0, null);
+                g2d.dispose();
+            }
         } else {
-            System.out.println(this + ": unknown buffer received " + buf);
+            LOGGER.warning(() -> this + ": unknown buffer received " + buf);
             return;
         }
 
-        Dimension d = component.getSize();
-        int x = 0;
-        int y = 0;
-        int w = d.width;
-        int h = d.height;
+        Dimension d = component != null ? component.getSize() : new Dimension(img.getWidth(), img.getHeight());
+        int w = d.width > 0 ? d.width : img.getWidth();
+        int h = d.height > 0 ? d.height : img.getHeight();
 
-        int new_font_size = w / 32;
-        if (new_font_size < 12)
-            new_font_size = 12;
-        if (font == null || new_font_size != font_size) {
-            font_size = new_font_size;
-            // Updated to standard logical font name for modern Java runtimes
-            font = new Font("SansSerif", Font.BOLD, font_size);
+        int newFontSize = w / 32;
+        if (newFontSize < 12) {
+            newFontSize = 12;
+        }
+        if (font == null || newFontSize != fontSize) {
+            fontSize = newFontSize;
+            font = new Font("SansSerif", Font.BOLD, fontSize);
         }
 
-        Graphics g = img.getGraphics();
-        g.drawImage(img, x, y, w, h, null);
-
-        /* render text on top */
-        if (text != null) {
-            double tw;
-            g.setFont(font);
-            g.setColor(Color.white);
-            FontMetrics fm = g.getFontMetrics();
-            tw = fm.stringWidth(text);
-            g.drawString(text, x + (int) ((w - tw) / 2), y + (int) (h * 0.85));
+        Graphics2D g2d = img.createGraphics();
+        
+        if (text != null && !text.isEmpty()) {
+            g2d.setFont(font);
+            g2d.setColor(Color.WHITE);
+            FontMetrics fm = g2d.getFontMetrics();
+            double tw = fm.stringWidth(text);
+            g2d.drawString(text, (int) ((w - tw) / 2), (int) (h * 0.85));
         }
 
-        g.dispose();
+        g2d.dispose();
+        buf.object = img;
     }
 
     @Override
     public boolean setProperty(String name, java.lang.Object value) {
-        if (name.equals("text")) {
-            text = value.toString();
+        if ("text".equals(name)) {
+            text = value != null ? value.toString() : null;
         } else {
             return super.setProperty(name, value);
         }
-
         return true;
     }
 
     @Override
     public java.lang.Object getProperty(String name) {
-        if (name.equals("text")) {
+        if ("text".equals(name)) {
             return text;
         } else {
             return super.getProperty(name);
