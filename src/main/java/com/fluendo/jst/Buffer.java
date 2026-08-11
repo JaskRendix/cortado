4fcee6cb32ab25e5ce8ac8e4755ac3a8c7b1e1ab
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-11 11:10:19 +0200

    modernize Buffer pool with ArrayDeque and remove unused live counter field

--- src/main/java/com/fluendo/jst/Buffer.java
+++ src/main/java/com/fluendo/jst/Buffer.java
@@ -18,90 +18,83 @@
 
 package com.fluendo.jst;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Buffer {
-  private static Stack pool = new Stack();
-  private static int live;
+    private static final Deque<Buffer> pool = new ArrayDeque<>();
 
-  public static final int FLAG_DISCONT = (1<<0);
-  public static final int FLAG_DELTA_UNIT = (1<<1);
+    public static final int FLAG_DISCONT = (1 << 0);
+    public static final int FLAG_DELTA_UNIT = (1 << 1);
 
-  public int flags;
+    public int flags;
 
-  public java.lang.Object object;
-  public byte[] data;
-  public int offset;
-  public int length;
-  public Caps caps;
+    public java.lang.Object object;
+    public byte[] data;
+    public int offset;
+    public int length;
+    public Caps caps;
 
-  public long time_offset;
-  public long timestamp;
-  public long timestampEnd;
-  public boolean duplicate = false;
+    public long time_offset;
+    public long timestamp;
+    public long timestampEnd;
+    public boolean duplicate = false;
 
-  public static Buffer create() {
-    Buffer result;
-    
-    try {
-      result = (Buffer) pool.pop();
+    public static Buffer create() {
+        Buffer result = pool.poll();
+        if (result == null) {
+            result = new Buffer();
+        }
+        result.time_offset = -1;
+        result.timestamp = -1;
+        result.timestampEnd = -1;
+        result.flags = 0;
+        result.object = null;
+        result.caps = null;
+
+        return result;
     }
-    catch (EmptyStackException e) {
-      result = new Buffer();
-      live++;
+
+    public Buffer() {
+        //System.out.println("new buffer");
     }
-    result.time_offset = -1;
-    result.timestamp = -1;
-    result.timestampEnd = -1;
-    result.flags = 0;
 
-    return result;
-  }
-
-  public Buffer() {
-    //System.out.println("new buffer");
-  }
-
-  public boolean isFlagSet (int flag) {
-    return (flags & flag) == flag;
-  }
-  public void setFlag (int flag, boolean val) {
-    if (val)
-      flags |= flag;
-    else
-      flags &= ~flag;
-  }
-
-  public void free() {
-    object = null;
-    caps = null;
-    pool.push(this);
-  }
-
-  public void ensureSize (int length)
-  {
-    if (data == null) {
-      data = new byte[length];
-      //System.out.println("create data "+pool.size()+" "+live+" "+length);
+    public boolean isFlagSet(int flag) {
+        return (flags & flag) == flag;
     }
-    else if (data.length < length) {
-      //System.out.println("expand buffer "+pool.size()+ " "+live+" "+data.length+" -> "+length);
-      data = new byte[length];
-    }
-  }
 
-  /**
-   * copies data into the buffer.
-   * 
-   * @param data: the bytearray with data to copy
-   * @param offset: offset in the bytearray of first byte
-   * @param length: length of data to copy
-   */
-  public void copyData (byte[] data, int offset, int length)
-  {
-    ensureSize(length);
-    System.arraycopy (data, offset, this.data, 0, length);
-    this.offset = 0;
-    this.length = length;
-  }
+    public void setFlag(int flag, boolean val) {
+        if (val) {
+            flags |= flag;
+        } else {
+            flags &= ~flag;
+        }
+    }
+
+    public void free() {
+        object = null;
+        caps = null;
+        pool.push(this);
+    }
+
+    public void ensureSize(int length) {
+        if (data == null || data.length < length) {
+            data = new byte[length];
+            //System.out.println("create data "+pool.size()+" "+length);
+        }
+    }
+
+    /**
+     * copies data into the buffer.
+     * 
+     * @param data: the bytearray with data to copy
+     * @param offset: offset in the bytearray of first byte
+     * @param length: length of data to copy
+     */
+    public void copyData(byte[] data, int offset, int length) {
+        ensureSize(length);
+        System.arraycopy(data, offset, this.data, 0, length);
+        this.offset = 0;
+        this.length = length;
+    }
 }
