08807450e9d5649450ec71fefce6884f79e0711c
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-12 13:58:14 +0200

    modernize Version conventions by declaring the class final with a private constructor and cleaning up formatting

--- src/main/java/com/fluendo/jheora/Version.java
+++ src/main/java/com/fluendo/jheora/Version.java
@@ -1,12 +1,12 @@
 /* Jheora
  * Copyright (C) 2004 Fluendo S.L.
- *  
+ * 
  * Written by: 2004 Wim Taymans <wim@fluendo.com>
- *   
+ *  
  * Many thanks to 
  *   The Xiph.Org Foundation http://www.xiph.org/
  * Jheora was based on their Theora reference decoder.
- *   
+ *  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License
  * as published by the Free Software Foundation; either version 2 of
@@ -24,21 +24,22 @@
 
 package com.fluendo.jheora;
 
-public class Version 
-{
-  public static final int VERSION_MAJOR = 3;
-  public static final int VERSION_MINOR = 2;
-  public static final int VERSION_SUB = 0;
+public final class Version {
+    public static final int VERSION_MAJOR = 3;
+    public static final int VERSION_MINOR = 2;
+    public static final int VERSION_SUB = 0;
 
-  private static final String VENDOR_STRING = "Xiph.Org libTheora I 20040317 3 2 0";
+    private static final String VENDOR_STRING = "Xiph.Org libTheora I 20040317 3 2 0";
 
-  public static String getVersionString()
-  {
-    return VENDOR_STRING;
-  }
+    private Version() {
+        // Prevent instantiation of utility class
+    }
 
-  public static int getVersionNumber()
-  {
-    return (VERSION_MAJOR<<16) + (VERSION_MINOR<<8) + (VERSION_SUB);
-  }
+    public static String getVersionString() {
+        return VENDOR_STRING;
+    }
+
+    public static int getVersionNumber() {
+        return (VERSION_MAJOR << 16) + (VERSION_MINOR << 8) + VERSION_SUB;
+    }
 }
