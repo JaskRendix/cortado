25b0db947f59aee30f2cd714a3cd99597db0977f
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-12 11:58:54 +0200

    modernize Result conventions by adding a private constructor to prevent instantiation of the constant utility class and cleaning up whitespace formatting

--- src/main/java/com/fluendo/jheora/Result.java
+++ src/main/java/com/fluendo/jheora/Result.java
@@ -1,12 +1,12 @@
 /* Jheora
  * Copyright (C) 2004 Fluendo S.L.
- *  
+ * 
  * Written by: 2004 Wim Taymans <wim@fluendo.com>
- *   
+ *  
  * Many thanks to 
  *   The Xiph.Org Foundation http://www.xiph.org/
  * Jheora was based on their Theora reference decoder.
- *   
+ *  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License
  * as published by the Free Software Foundation; either version 2 of
@@ -25,12 +25,16 @@
 package com.fluendo.jheora;
 
 public final class Result {
-  public static final int FAULT     = -1;
-  public static final int EINVAL    = -10;
-  public static final int BADHEADER = -20;
-  public static final int NOTFORMAT = -21;
-  public static final int VERSION   = -22;
-  public static final int IMPL      = -23;
-  public static final int BADPACKET = -24;
-  public static final int NEWPACKET = -25;
+    private Result() {
+        // Prevent instantiation of constant utility class
+    }
+
+    public static final int FAULT     = -1;
+    public static final int EINVAL    = -10;
+    public static final int BADHEADER = -20;
+    public static final int NOTFORMAT = -21;
+    public static final int VERSION   = -22;
+    public static final int IMPL      = -23;
+    public static final int BADPACKET = -24;
+    public static final int NEWPACKET = -25;
 }
