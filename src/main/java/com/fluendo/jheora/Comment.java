76f4f9cd04e709139e79c29f0724bdbff76201fd
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-12 12:06:22 +0200

    modernize Comment conventions by making the class final and cleaning up whitespace formatting

--- src/main/java/com/fluendo/jheora/Comment.java
+++ src/main/java/com/fluendo/jheora/Comment.java
@@ -1,12 +1,12 @@
 /* Jheora
  * Copyright (C) 2004 Fluendo S.L.
- *  
+ * 
  * Written by: 2004 Wim Taymans <wim@fluendo.com>
- *   
+ *  
  * Many thanks to 
  *   The Xiph.Org Foundation http://www.xiph.org/
  * Jheora was based on their Theora reference decoder.
- *   
+ *  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License
  * as published by the Free Software Foundation; either version 2 of
@@ -24,13 +24,12 @@
 
 package com.fluendo.jheora;
 
-public class Comment {
-  String[] user_comments;
-  String vendor;
+public final class Comment {
+    String[] user_comments;
+    String vendor;
 
-
-  public void clear() {
-    user_comments = null;
-    vendor = null;
-  }
+    public void clear() {
+        user_comments = null;
+        vendor = null;
+    }
 }
