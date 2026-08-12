8e8e5310684a37b68388b5202e1f9348fd0b5407
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-12 12:08:36 +0200

    modernize Colorspace conventions by making the class final, updating the constant array name spaces to UPPER_CASE (SPACES), and standardizing formatting

--- src/main/java/com/fluendo/jheora/Colorspace.java
+++ src/main/java/com/fluendo/jheora/Colorspace.java
@@ -1,12 +1,12 @@
 /* Jheora
  * Copyright (C) 2004 Fluendo S.L.
- *  
+ * 
  * Written by: 2004 Wim Taymans <wim@fluendo.com>
- *   
+ *  
  * Many thanks to 
  *   The Xiph.Org Foundation http://www.xiph.org/
  * Jheora was based on their Theora reference decoder.
- *   
+ *  
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public License
  * as published by the Free Software Foundation; either version 2 of
@@ -24,17 +24,17 @@
 
 package com.fluendo.jheora;
 
-public class Colorspace {
-  public static final Colorspace UNSPECIFIED = new Colorspace ();
-  public static final Colorspace ITU_REC_470M = new Colorspace ();
-  public static final Colorspace ITU_REC_470BG = new Colorspace ();
+public final class Colorspace {
+    public static final Colorspace UNSPECIFIED = new Colorspace();
+    public static final Colorspace ITU_REC_470M = new Colorspace();
+    public static final Colorspace ITU_REC_470BG = new Colorspace();
 
-  public static final Colorspace[] spaces = {
-    UNSPECIFIED,
-    ITU_REC_470M,
-    ITU_REC_470BG
-  };
+    public static final Colorspace[] SPACES = {
+        UNSPECIFIED,
+        ITU_REC_470M,
+        ITU_REC_470BG
+    };
 
-  private Colorspace() {
-  }
+    private Colorspace() {
+    }
 }
