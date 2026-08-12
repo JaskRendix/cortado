a149353f7043114339d5679d10c21e88f34706cb
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-08-12 15:33:40 +0200

    update KateCurveTypeTest to align with enum refactoring

--- src/test/java/com/fluendo/jkate/KateCurveTypeTest.java
+++ src/test/java/com/fluendo/jkate/KateCurveTypeTest.java
@@ -10,22 +10,20 @@ class KateCurveTypeTest {
     @Test
     @DisplayName("Factory: Valid indices return correct curve type instances")
     void testCreateCurveTypeValid() throws KateException {
-        KateCurveType noneType = KateCurveType.CreateCurveType(0);
+        KateCurveType noneType = KateCurveType.createCurveType(0);
         assertNotNull(noneType);
         assertEquals(KateCurveType.KATE_CURVE_NONE, noneType);
-        assertEquals(KateCurveType.kate_curve_none, noneType);
 
-        KateCurveType bsplineType = KateCurveType.CreateCurveType(5);
+        KateCurveType bsplineType = KateCurveType.createCurveType(5);
         assertNotNull(bsplineType);
         assertEquals(KateCurveType.KATE_CURVE_BSPLINE, bsplineType);
-        assertEquals(KateCurveType.kate_curve_bspline, bsplineType);
     }
 
     @Test
     @DisplayName("Factory: Out of bounds negative index throws KateException")
     void testCreateCurveTypeNegativeIndex() {
         assertThrows(KateException.class, () -> {
-            KateCurveType.CreateCurveType(-1);
+            KateCurveType.createCurveType(-1);
         });
     }
 
@@ -33,7 +31,7 @@ class KateCurveTypeTest {
     @DisplayName("Factory: Out of bounds positive index throws KateException")
     void testCreateCurveTypeOutOfBoundsIndex() {
         assertThrows(KateException.class, () -> {
-            KateCurveType.CreateCurveType(6);
+            KateCurveType.createCurveType(6);
         });
     }
 }
